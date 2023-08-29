2617fc4c9f69ee349986be812196cac4bc24a4de
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-08-29 20:54:00 +0300

    fix: added battery optimization ignoring

--- app/src/main/kotlin/me/y9san9/lifetime/android/MainActivity.kt
+++ app/src/main/kotlin/me/y9san9/lifetime/android/MainActivity.kt
@@ -1,9 +1,13 @@
 package me.y9san9.lifetime.android
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.PowerManager
+import android.provider.Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.isSystemInDarkTheme
@@ -12,12 +16,14 @@ import me.y9san9.lifetime.android.integration.mainViewModel
 import me.y9san9.lifetime.compose.AppTheme
 import me.y9san9.lifetime.compose.integration.MainContent
 
+
 class MainActivity : ComponentActivity() {
     private val viewModel by mainViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, /* decorFitsSystemWindows = */ false)
+        checkBatteryOptimizations()
 
         setContent {
             AppTheme(isSystemInDarkTheme()) {
@@ -41,6 +47,22 @@ class MainActivity : ComponentActivity() {
             ForegroundService
                 .moveToForegroundIntent(this)
                 .apply(::startForegroundServiceCompat)
+        }
+    }
+
+    /**
+     * This really sucks, but Huawei kills foreground services LoL
+     */
+    private fun checkBatteryOptimizations() {
+        val packageName = applicationContext.packageName
+
+        val pm = applicationContext.getSystemService(POWER_SERVICE) as PowerManager
+
+        if (!pm.isIgnoringBatteryOptimizations(packageName)) {
+            val intent = Intent()
+            intent.setAction(ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+            intent.setData(Uri.parse("package:$packageName"))
+            startActivity(intent)
         }
     }
 }
