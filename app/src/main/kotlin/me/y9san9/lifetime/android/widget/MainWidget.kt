41a6b7bdca0d0ea54b9b7c7c72d90e17483964c4
Alex Sokol / y9san9 <y9san9@gmail.com>
2023-09-12 09:49:28 +0300

    feat: added rounded corners

--- app/src/main/kotlin/me/y9san9/lifetime/android/widget/MainWidget.kt
+++ app/src/main/kotlin/me/y9san9/lifetime/android/widget/MainWidget.kt
@@ -1,11 +1,21 @@
 package me.y9san9.lifetime.android.widget
 
 import android.content.Context
+import android.graphics.drawable.ShapeDrawable
+import android.graphics.drawable.shapes.RoundRectShape
+import android.os.Build
+import androidx.annotation.FloatRange
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.ColorUtils
+import androidx.core.graphics.drawable.toBitmap
 import androidx.glance.*
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.*
@@ -20,8 +30,10 @@ import me.y9san9.lifetime.R
 import me.y9san9.lifetime.android.extensions.toggleCountdownFromWidget
 import me.y9san9.lifetime.looper.looper
 
-class MainWidget : GlanceAppWidget() {
+object MainWidget : GlanceAppWidget() {
     private val looper = di.looper
+    @OptIn(DelicateCoroutinesApi::class)
+    private val scope = GlobalScope + CoroutineName("Main LifeTime Widgets Scope")
 
     override val sizeMode = SizeMode.Single
 
@@ -52,8 +64,9 @@ class MainWidget : GlanceAppWidget() {
 
         Box(
             modifier = GlanceModifier
-                .fillMaxSize()
                 .appWidgetBackground()
+                .fillMaxSize()
+                .cornerRadiusCompat(10.dp, GlanceTheme.colors.background)
                 .background(GlanceTheme.colors.background)
                 .clickable(onClick),
             contentAlignment = Alignment.Center
@@ -80,14 +93,40 @@ class MainWidget : GlanceAppWidget() {
         }
     }
 
-    companion object {
-        @OptIn(DelicateCoroutinesApi::class)
-        private val scope = GlobalScope + CoroutineName("Main LifeTime Widgets Scope")
-
-        fun update(context: Context) {
-            scope.launch {
-                MainWidget().updateAll(context)
-            }
+    fun update(context: Context) {
+        scope.launch {
+            MainWidget.updateAll(context)
         }
+    }
+}
+
+/**
+ * Adds rounded corners for the current view.
+ *
+ * On S+ it uses [GlanceModifier.cornerRadius]
+ * on <S it creates [ShapeDrawable] and sets background
+ *
+ * @param cornerRadius [Int] radius set to all corners of the view.
+ * @param color [Int] value of a color that will be set as background
+ * @param backgroundAlpha [Float] value of an alpha that will be set to background color - defaults to 1f
+ */
+@Composable
+private fun GlanceModifier.cornerRadiusCompat(
+    cornerRadius: Dp,
+    color: ColorProvider,
+    @FloatRange(from = 0.0, to = 1.0) backgroundAlpha: Float = 1f,
+): GlanceModifier {
+    val actualColor = color.getColor(LocalContext.current)
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        background(actualColor.copy(alpha = backgroundAlpha))
+            .cornerRadius(cornerRadius)
+    } else {
+        val cornerRadiusFloat = cornerRadius.value * LocalContext.current.resources.displayMetrics.density
+        val radii = FloatArray(8) { cornerRadiusFloat }
+        val shape = ShapeDrawable(RoundRectShape(radii, null, null))
+        shape.paint.color = ColorUtils.setAlphaComponent(actualColor.toArgb(), (255 * backgroundAlpha).toInt())
+        val bitmap = shape.toBitmap(width = 150, height = 75)
+        this.background(BitmapImageProvider(bitmap))
     }
 }
--- app/src/main/kotlin/me/y9san9/lifetime/android/widget/MainWidgetReceiver.kt
+++ app/src/main/kotlin/me/y9san9/lifetime/android/widget/MainWidgetReceiver.kt
@@ -3,5 +3,5 @@ package me.y9san9.lifetime.android.widget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
 
 class MainWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget = MainWidget()
+    override val glanceAppWidget = MainWidget
 }
